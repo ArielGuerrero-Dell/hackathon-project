5a1282c5b1aa635bc6ef4c1c940ed139d3c9cbd4
ariel_guerrero-dell <Ariel_Guerrero@Dell.com>
2021-06-22 10:57:04 -0500

    nit commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackathon-project';
+  search_item!: string;
+  submit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AccordionModule } from 'primeng/accordion';     //accordion and accordion tab
+import { MenuItem } from 'primeng/api';                  //api
+
+import { ButtonModule } from 'primeng/button';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    FormsModule,
+    AccordionModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
