2db48573116428b605cd0a1b46924de2a231cdad
Guerrero <Ariel_Guerrero@Dell.com>
2021-06-29 09:03:00 -0500

    fixed gramatical erreros in file names and updated colors to match Dell theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AccordionModule } from 'ngx-bootstrap/accordion';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TopTenAccordionComponent } from './top-ten-accordion/top-ten-accordion.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
