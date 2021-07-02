e120b8f7f44f888592da0f6e453fb67b75f287cc
Ariel_Guerrero <ariel_guerrero@dell.com>
2021-07-02 18:15:13 -0500

    speech to text implementation start; service gets speech correctly.... need to link to front end text field

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { VoiceRecognitionService } from '../voice-recognition.service'
+
+declare var webkitSpeechRecognition: any;
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
+  providers: [VoiceRecognitionService]
 })
 export class NavBarComponent implements OnInit {
+
+  recognition = new webkitSpeechRecognition();
+
+  public text = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  startService() {
+    this.recognition.start();
+
+    console.log("Speech recognition started");
+
+    this.recognition.onresult = function (event: any) {
+      this.text = event.results[0][0].transcript;
+      console.log(this.text);
+    }
+  }
+
 
 }
